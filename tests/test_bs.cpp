353c8f68b6101c79a52864c7fd7abbc409bf25a2
GAprea <96022352+GAprea@users.noreply.github.com>
2025-07-01 08:13:13 +0200

    Delete tests/test_bs.cpp

--- tests/test_bs.cpp
+++
@@ -1,8 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-#include "fxoptions/bs.hpp"
-
-TEST_CASE("BS price vs known value", "[bs]") {
-    double p = fx::black_scholes_price(1.2,1.25,1.0,0.15,0.05,0.02);
-    REQUIRE( Approx(p).margin(1e-6) == 0.0644887 );
-}
